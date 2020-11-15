a1b401bd96017186a5a13367d67aca13a8a9bcef
J. Szucs <szujoz95@gmail.com>
2020-11-15 18:29:04 +0100

    CRC8 and Escape encoder facade classes are added. Command Packer refactor: new private functions, pack and unpack functions refactored.

--- RobotDiagTool/DiagTool_Libs/robot_commands/commandpacker.cpp
+++ RobotDiagTool/DiagTool_Libs/robot_commands/commandpacker.cpp
@@ -1,6 +1,7 @@
 #include "commandpacker.h"
 #include "robotcommand.h"
 #include "EscapeEncoder.h"
+#include <QtDebug>
 
 CommandPacker* CommandPacker::instance_ = NULL;
 
@@ -19,93 +20,170 @@ void CommandPacker::RegisterCommand(const CommandID id, IRobotCommand *cmd)
     factory.RegisterCommand(id, cmd);
 }
 
-QByteArray CommandPacker::Pack(QByteArray &message)
+QByteArray CommandPacker::Pack(QByteArray &command_bytes)
 {
-    QByteArray           msg = message;
-    QByteArray           enc_msg;
-    std::vector<uint8_t> orig_message;
-    std::vector<uint8_t> encoded_message;
-    uint8_t              temp[msg.size()*2];
+    QByteArray           command = command_bytes;
+    QByteArray           message;
+    std::vector<uint8_t> commandBytes;
+    std::vector<uint8_t> encodedBytes;
+    uint8_t              crcByte;
 
-    // CRC
-    for (size_t i = 0; i < (size_t)msg.size(); i++)
-    {
-        orig_message.push_back(static_cast<uint8_t>(msg.at(i)));
-    }
+    // Step 1: Add CRC
+    commandBytes = Conv_QByteArray_To_U8Vector(command);
+    crcByte = crc->CalculateCrC(commandBytes);
+    commandBytes.push_back(crcByte);
 
-    orig_message.push_back(crc->GetBlockCrc(orig_message.data(), orig_message.size()));
+    // Step 2: Encode
+    encodedBytes = coder->Encode(commandBytes);
 
-    // Encode
-    size_t enc_size = encoded_message.size();
-    encoded_message.reserve(orig_message.size()*2);
-    coder->Encode(orig_message.data(),    orig_message.size(),
-                  temp, enc_size);
+    // Step 3: append '\n'
+    encodedBytes.push_back('\n');
+    message = Conv_U8Vector_To_QByteArray(encodedBytes);
 
-    // \n
-   // encoded_message.push_back('\n');
-
-    for (size_t i = 0; i < enc_size; i++)
-    {
-        enc_msg.append(temp[i]);
-    }
-    enc_msg.append('\n');
-
-    return enc_msg;
+    return message;
 }
 
 void CommandPacker::Unpack(QByteArray &message)
 {
-    CommandID            cmdId = CommandID::eUnknown;
-    IRobotCommand*       cmd;
-    std::vector<uint8_t> coded_message;
-    size_t               coded_size = 0;
-    std::vector<uint8_t> decoded_message;
-    size_t               decoded_size = 0;
-    bool                 isCrcOk = false;
-    QByteArray           messageToBeProcessed;
+    IRobotCommand*       cmdObj = NULL;
+    QByteArray           codedMessage = message;
+    QByteArray           decodedMessage;
+    QByteArray           payload;
+    std::vector<uint8_t> codedBytes;
+    std::vector<uint8_t> decodedBytes;
+    bool                 successfulExtraction = false;
 
-    // Determine array sizes
-    coded_size = message.size();
-    decoded_size = coded_size;      // The decoded bytes will be surely shorter.
+    // Step 1: Decode
+    codedBytes = Conv_QByteArray_To_U8Vector(codedMessage);
+    decodedBytes = coder->Decode(codedBytes);
+    decodedMessage = Conv_U8Vector_To_QByteArray(decodedBytes);
 
-    for (size_t i=0; i < coded_size; i++)
+    // Step 2: Check CRC
+    if(crc->IsCrcValid(decodedBytes) == true)
     {
-        coded_message.push_back(static_cast<uint8_t>(message[i]));
+        payload = ExtractPayload(&successfulExtraction, &cmdObj, decodedMessage);
+
+        if (successfulExtraction == true)
+        {
+           cmdObj->RxProcessing(payload);
+        }
     }
-    decoded_message.reserve(decoded_size);
-
-    // Decode the message.
-    coder.get()->Decode(coded_message.data(), coded_size, decoded_message.data(), decoded_size);
-
-    // Check CRC
-    isCrcOk = crc.get()->CheckBlockCrc(decoded_message.data(), decoded_size);
-
-    if(isCrcOk == true)
+    else
     {
-        // Extract the command ID and get the object from the factory.
-        cmdId = static_cast<CommandID>(message[0] - 0x00);
-        cmd = factory.CreateCommand(cmdId);
-
-        for(size_t i = 0; i < decoded_size; i++)
-        {
-           messageToBeProcessed.append(decoded_message[i]);
-        }
-        messageToBeProcessed.remove(0,1);
-        messageToBeProcessed.remove(messageToBeProcessed.size()-1,1);
-
-        // Command object was found. Remove the Command ID byte and process the bytes.
-        if(cmd != NULL)
-        {
-            cmd->RxProcessing(messageToBeProcessed);
-        }
+        qDebug() << "Message crc error: " << QString(decodedMessage.toHex(' '));
     }
 }
 
 CommandPacker::CommandPacker()
 {
-    coder = std::make_unique<EscapeEncoder>();
+    coder = std::make_unique<EscapeEncoder_F>();
+    crc = std::make_unique<CRC8_F>();
+}
+
+std::vector<uint8_t> CommandPacker::Conv_QByteArray_To_U8Vector(QByteArray& bytes)
+{
+    std::vector<uint8_t> vector;
+    QByteArray byteArray = bytes;
+
+    for (int i = 0; i < byteArray.size(); i++)
+    {
+        vector.push_back(static_cast<uint8_t>(byteArray.at(i)));
+    }
+
+    return vector;
+}
+
+QByteArray CommandPacker::Conv_U8Vector_To_QByteArray(std::vector<uint8_t>& vector)
+{
+    std::vector<uint8_t> _vector = vector;
+    QByteArray byteArray;
+
+    for(size_t i = 0; i < _vector.size(); i++)
+    {
+       byteArray.append(_vector[i]);
+    }
+
+    return byteArray;
+}
+
+QByteArray CommandPacker::ExtractPayload(bool* success, IRobotCommand** cmd, QByteArray const bytes)
+{
+    QByteArray     commandBytes = bytes;
+    CommandID      commandId = CommandID::eUnknownCommand;
+
+    // Extract the command ID and get the object from the factory.
+    commandId = static_cast<CommandID>(commandBytes[0] - 0x00);
+    *cmd = factory.CreateCommand(commandId);
+
+    if(*cmd != NULL)
+    {
+        *success = true;
+
+        // Remove the Command ID and the CRC bytes.
+        commandBytes.remove(0,1);
+        commandBytes.remove(commandBytes.size()-1,1);
+    }
+    else
+    {
+        *success = false;
+        qDebug() << "Unknown Command ID: " << commandId;
+    }
+
+    return commandBytes;
+}
+
+CRC8_F::CRC8_F()
+{
     crc = std::make_unique<Crc8>();
 }
 
+uint8_t CRC8_F::CalculateCrC(std::vector<uint8_t> const crc_bytes)
+{
+    uint8_t calculated_crc = 0;
 
+    calculated_crc = crc->GetBlockCrc(crc_bytes.data(), crc_bytes.size());
 
+    return calculated_crc;
+}
+
+bool CRC8_F::IsCrcValid(std::vector<uint8_t> const crc_bytes)
+{
+    bool isCrcOk = false;
+
+    isCrcOk = crc->CheckBlockCrc(crc_bytes.data(), crc_bytes.size());
+
+    return isCrcOk;
+}
+
+EscapeEncoder_F::EscapeEncoder_F()
+{
+    coder = std::make_unique<EscapeEncoder>();
+}
+
+std::vector<uint8_t> EscapeEncoder_F::Encode(const std::vector<uint8_t> bytes)
+{
+    std::vector<uint8_t> originalBytes = bytes;
+    std::vector<uint8_t> encodedBytes;
+    uint8_t encodedArray[originalBytes.size()*2]; // Reserve twice as many bytes to even store all as escaped characters.
+    size_t encodedLenght = 0;
+
+    coder->Encode(originalBytes.data(), originalBytes.size(), encodedArray, encodedLenght);
+
+    encodedBytes = std::vector<uint8_t>(&encodedArray[0], &encodedArray[encodedLenght]);
+
+    return encodedBytes;
+}
+
+std::vector<uint8_t> EscapeEncoder_F::Decode(const std::vector<uint8_t> encoded_bytes)
+{
+    std::vector<uint8_t> encodedBytes = encoded_bytes;
+    std::vector<uint8_t> decodedBytes;
+    uint8_t decodedArray[encodedBytes.size()];  // Reserve the same number of bytes, the decoded message less or equal in size.
+    size_t decodedLenght = 0;
+
+    coder->Decode(encodedBytes.data(), encodedBytes.size(), decodedArray, decodedLenght);
+
+    decodedBytes = std::vector<uint8_t>(&decodedArray[0], &decodedArray[decodedLenght]);
+
+    return decodedBytes;
+}
--- RobotDiagTool/DiagTool_Libs/robot_commands/commandpacker.h
+++ RobotDiagTool/DiagTool_Libs/robot_commands/commandpacker.h
@@ -11,6 +11,9 @@
 #include "BinaryEncoder.h"
 #include "commandfactory.h"
 
+class CRC8_F;
+class EscapeEncoder_F;
+
 class CommandPacker : public ICommandPacker
 {
     Q_OBJECT
@@ -18,16 +21,45 @@ public:
     static CommandPacker *GetInstance();
 
     virtual void RegisterCommand(CommandID const id, IRobotCommand* cmd) override;
-    virtual QByteArray Pack(QByteArray& message) override;
+    virtual QByteArray Pack(QByteArray& command_bytes) override;
     virtual void Unpack(QByteArray& message) override;
 
 private:
     static CommandPacker* instance_;
     CommandFactory factory;
-    std::unique_ptr<BinaryEncoder> coder;
-    std::unique_ptr<Crc8> crc;
+    std::unique_ptr<EscapeEncoder_F> coder;
+    std::unique_ptr<CRC8_F> crc;
 
     CommandPacker();
+
+    std::vector<uint8_t> Conv_QByteArray_To_U8Vector(QByteArray& bytes);
+    QByteArray Conv_U8Vector_To_QByteArray(std::vector<uint8_t>& vector);
+
+    QByteArray ExtractPayload(bool* success, IRobotCommand** cmd, QByteArray const bytes);
+};
+
+class CRC8_F  // Facade for crc8.h funcitons.
+{
+public:
+    CRC8_F();
+
+    uint8_t CalculateCrC(std::vector<uint8_t> const crc_bytes);
+    bool IsCrcValid(std::vector<uint8_t> const crc_bytes);
+
+private:
+    std::unique_ptr<Crc8> crc;
+};
+
+class EscapeEncoder_F // Facade for EscapeEncoder.h functions
+{
+public:
+    EscapeEncoder_F();
+
+    std::vector<uint8_t> Encode(std::vector<uint8_t> const bytes);
+    std::vector<uint8_t> Decode(std::vector<uint8_t> const encoded_bytes);
+
+private:
+    std::unique_ptr<BinaryEncoder> coder;
 };
 
 #endif // COMMANDPACKER_H
