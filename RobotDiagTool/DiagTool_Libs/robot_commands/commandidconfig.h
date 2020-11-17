c596fed55533760b8d24e3223bdb694d9f7e65b6
J. Szucs <szujoz95@gmail.com>
2020-11-17 23:07:21 +0100

    Telemtry ID and COnfigParam ID are introduced.

--- RobotDiagTool/DiagTool_Libs/robot_commands/commandidconfig.h
+++ RobotDiagTool/DiagTool_Libs/robot_commands/commandidconfig.h
@@ -5,8 +5,23 @@ enum CommandID
 {
     eText = 0x00,
     eDummyData = 0x01,
+    eTelemetry = 0x02,
+    eConfigParam = 0x03,
 
     eUnknownCommand = 0xFF
+};
+
+enum TelemetryID
+{
+    eEncoderSpeed = 0x00,
+    eRemoteChannels = 0x01,
+
+    eUnknownTelemetry = 0xFF
+};
+
+enum ConfigParamID
+{
+    e7SegNum = 0x0001,
 };
 
 #endif // COMMANDIDCONFIG_H
